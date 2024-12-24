819056562640931dc1176904e3d475f9295f3d8a
Jeferson Pinheiro de Jesus <iservice.pinheiro@gmail.com>
2024-12-24 00:40:56 -0300

    Initial commit

---
+++ src/main/java/br/com/pinedev/orderms/service/OrderService.java
@@ -0,0 +1,60 @@
+package br.com.pinedev.orderms.service;
+
+
+import br.com.pinedev.orderms.controller.dto.OrderResponse;
+import br.com.pinedev.orderms.entity.OrderEntity;
+import br.com.pinedev.orderms.entity.OrderItem;
+import br.com.pinedev.orderms.listener.dto.OrderCreatedEvent;
+import br.com.pinedev.orderms.repository.OrderRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void save(OrderCreatedEvent event) {
+
+        var entity = new OrderEntity();
+
+        entity.setOrderId(event.codigoPedido());
+        entity.setCustomerId(event.codigoCliente());
+        entity.setItems(getOrderItems(event));
+        entity.setTotal(getTotal(event));
+
+        orderRepository.save(entity);
+
+    }
+
+    public Page<OrderResponse> findAllByCustomerId(Long customerId, PageRequest pageRequest) {
+        var orders = orderRepository.findAllByCustomerId(customerId,pageRequest);
+
+        return orders.map(OrderResponse::fromEntity);
+    }
+
+
+
+    private BigDecimal getTotal(OrderCreatedEvent event) {
+        return event.itens()
+                .stream()
+                .map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade())))
+                .reduce(BigDecimal::add)
+                .orElse(BigDecimal.ZERO);
+    }
+
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
+        return event.itens().stream()
+                .map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco()))
+                .toList();
+    }
+
+}
